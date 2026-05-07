71709567c011ef75fac11f92bbed0ea0e50c97d6
Professor <Professor@SN-407020.edu.fiesc.com.br>
2026-05-06 21:54:52 -0300

    começamos a fazer o inicio do jogo

---
+++ a_revolta_das_princesas.py
@@ -0,0 +1,28 @@
+print ("A revolta das princesas")
+print (" olá jogador(a), seja bem vindo!")
+print (" esse jogo representa o combate das princesas")
+print ("que usaram seus poderes para combater")
+print (" os jogadores teram 100 hp")
+print ("os nomes dos personagens são Branca de leite e a Pé solto")
+print (" quem atacará primeiro será a Branca de leite")
+print("a Branca de leite irá usar a arma que atira maçãs, e o feitiço do sono da morte")
+print(" a branca de leite tirou 10 de hp")
+print("o segundo personagem que ira atacar será a Pé solto")
+print(" a Pé solto ira atacar tenis, e tera o feitiço de transformar o seu adversario em abobora")
+print("a pé solto tirou 20 hp")
+print("nome1=Branca de leite, nome2=Pé solto")
+
+print("nome do jogador 1= Branca de leite")
+print("nome do jogador 2= Pé solto ")
+hp1=100
+hp2=100
+dano1=10
+dano2=20
+soma1=100-10
+soma2=100-20
+print("quem ganhou foi a pé solto")
+
+
+
+
+
