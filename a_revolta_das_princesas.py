5ec8f78d3e243647b6676f14ce2aff4c4ab17eda
Professor <Professor@SN-407020.edu.fiesc.com.br>
2026-05-13 21:49:58 -0300

    continuação do jogo

--- a_revolta_das_princesas.py
+++ a_revolta_das_princesas.py
@@ -22,6 +22,120 @@ soma1=100-10
 soma2=100-20
 print("quem ganhou foi a pé solto")
 
+hp1 = 100
+hp2 = 100 
+
+while hp1 > 0 and hp2 > 0:
+    print("branca de leite atacou!")
+    hp2 = hp2 -10
+    print(" HP da pe solto:", hp2)
+
+    if hp2 <= 0:
+        print("branca de leite venceu")
+        break
+        
+    print("pe solto atacou!")
+    hp1 =hp1 - 20
+    print("hp da branca de leite:", hp1)
+
+    if hp1 <= 0:
+        print("pe solto venceu")
+        break
+
+print ("A revolta das princesas")
+
+hp1 =100
+hp2 =100
+
+for rodada in range(1,4):
+
+    print("\n--- rodada", rodada, "---")
+
+    print("escolha o ataque da branca de leite")
+    print("1 - magia")
+    print("2 - chute")
+
+    escolha = int(input("digite o número do ataque: "))
+
+    if escolha == 1 :
+        dano = 20
+        print ("branca de leite usou magia")
+    else:
+        dano = 10
+        print (" branca de leite usou chute")
+
+    hp2 = hp2 - dano
+
+    print("HP da pé solto:", hp2)
+
+    print("\nfim da batalha")
+
+    if hp2 <= 0:
+        print(" branca de leite venceu")
+        print(" fim da batalha")
+        break
+
+    else:
+        print(" pé solto venceu")
+
+    print (" A revolta das princesas")
+    hp2 = 100
+
+    for rodada in range(1,6):
+
+        print("\n--- rodada", rodada, "---")
+
+        print("escolha o ataque:")
+        print("1 - magia")
+        print("2 - chute")
+        print("3 - espada")
+        print("4 - fogo")
+
+        escolha = int(input(" digite o numero do ataque: "))
+
+        if escolha == 1:
+            dano=20
+            print("branca de leite usou magia")
+
+        elif escolha == 2:
+            dano = 20
+            print(" branca de leite usou chute")
+
+        elif escolha == 3:
+            dano = 30
+            print(" branca de leite usou espada")
+
+        elif escolha == 4:
+            dano = 40
+            print("branca de leite usou fogo")
+
+        else:
+            dano = 0
+            print(" ataque invalido")
+
+            hp2 = hp2 - dano
+
+            print("HP da pe solto:",hp2)
+
+            if hp2 <= 0:
+                break
+
+            print("\nfim da batalha")
+
+            if hp2 <= 0:
+                print(" branca de leite venceu")
+
+            elif hp1 <= 0:
+                print("pe solto venceu")
+            
+
+
+  
+
+    
+
+ 
+
 
 
 
